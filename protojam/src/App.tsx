6cf22c42219dc8eebfcf65d97854d957e92be024
Victor dS <victordespirlet@gmail.com>
2026-02-19 10:52:48 +0100

    build an app

--- protojam/src/App.tsx
+++ protojam/src/App.tsx
@@ -1,11 +1,24 @@
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { exercises } from "./data/exercices";
+import Exercices from "./components/Exercices";
 
 function App() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [score, setScore] = useState(0);
+  const currentExercise = exercises[currentIndex]
 
+  const handleCorrect = () => {
+    setScore(score + 1);
+    setCurrentIndex(currentIndex + 1)
+  }
+  
   return (
     <>
+      <p>Current Score: {score}</p>
+      <Exercices exercise={currentExercise} onCorrect={handleCorrect}/>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ protojam/src/components/Exercices.tsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import { Exercise, type CodeBlock } from "../types";
+import CodeBlock from "./CodeBlock";
+import DropZone from "./DropZone";
+import { DndContext } from "@dnd-kit/core";
+import type { DragEndEvent } from "@dnd-kit/core";
+
+
+interface ExerciseProps {
+  exercise: Exercise;
+  onCorrect: () => void;
+}
+
+function Exercices({ exercise, onCorrect }: ExerciseProps) {
+  const [droppedBlocks, setDroppedBlocks] = useState<CodeBlock[]>([]);
+
+  const handleDragEnd = (event: DragEndEvent) => {
+    const draggedBlockId = event.active.id
+    const foundBlock = exercise.blocks.find(CodeBlock => CodeBlock.id === draggedBlockId)
+    if(foundBlock){
+        setDroppedBlocks(prev => [...prev, foundBlock])
+    }
+  };
+
+  const isCorrect = droppedBlocks.map(myBlock => myBlock.id).join(",") === exercise.answer.join(",")
+
+  return (
+    <DndContext onDragEnd={handleDragEnd}>
+      <div>
+        <div>
+          <div>{exercise.question}</div>
+        </div>
+        {exercise.blocks.map((block) => (
+          <CodeBlock key={block.id} block={block} />
+        ))}
+
+        <DropZone droppedBlocks={droppedBlocks} />
+        <button onClick={() => {if(isCorrect){onCorrect()}}}>Check Answer</button>
+      </div>
+    </DndContext>
+  );
+}
+
+export default Exercices;
